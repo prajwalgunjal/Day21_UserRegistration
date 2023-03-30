454f78f30951fb0fbaac4ce13ef31b4e958c01b6
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-30 19:35:08 +0530

    UC2 Added :Last name checked

--- main/java/com/bridgelabz/UserRegistration.java
+++ main/java/com/bridgelabz/UserRegistration.java
@@ -4,12 +4,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public boolean validateFirstName(String value) {
+    public boolean validate(String Firstname) {
         Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
-        Matcher matcher = pattern.matcher(value);
+        Matcher matcher = pattern.matcher(Firstname);
         if (matcher.matches())
             return true;
         else
             return false;
     }
+
+    public boolean validateLastname(String Lastname){
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
+        Matcher matcher = pattern.matcher(Lastname);
+        if(matcher.matches())
+            return true;
+        else
+            return false;
+    }
+
+
+
 }
--- test/java/com/bridgelabz/UserRegistrationTest.java
+++ test/java/com/bridgelabz/UserRegistrationTest.java
@@ -5,16 +5,28 @@ import org.junit.jupiter.api.*;
 
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserRegistrationTest {
     static UserRegistration userRegistration;
     static String name;
 
-
+@BeforeAll
+public static void init(){
+    System.out.println("Before all");
+    userRegistration = new UserRegistration();
+}
 @Test
     void givenFirstnameshouldreturnTrue(){
-        boolean value = userRegistration.validateFirstName("Prajwal");
+        boolean value = userRegistration.validate("Prajwal");
         Assertions.assertTrue(value);
-    System.out.println("Successfully passed UC1");
+    System.out.println("First Name Checked:- Successfully passed UC1");
+    }
+    @Test
+    void givenLastNameShouldReturnTrue(){
+        boolean LastName = userRegistration.validateLastname("Gunjal");
+        Assertions.assertTrue(LastName);
+        System.out.println("Last Name Checked :- Successfully passed UC2");
     }
 }
