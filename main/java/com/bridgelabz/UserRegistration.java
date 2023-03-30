0a7ea08e9649fe3257de1ea6653a33f8e250a8fd
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-30 19:19:22 +0530

    Welcome To User registration

---
+++ main/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration System");
+    }
+}
