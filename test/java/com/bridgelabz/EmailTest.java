56ffc007985291dca97c48c58621c6e27bdda3ff
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-04-03 12:53:59 +0530

    UC11 updated

--- test/java/com/bridgelabz/EmailTest.java
+++ test/java/com/bridgelabz/EmailTest.java
@@ -27,7 +27,7 @@ public class EmailTest {
     }
     @Parameterized.Parameters
     public static Collection input(){
-        return Arrays.asList(new Object[][]{{"abc.100@abc.com.au",true},{"abc@yahoo.com",true},{"abc@yahoo.com",true},{"abc-100@yahoo.com",true},{"abc@1.com",true},{"abc111@abc.com",true},{"abc-100@abc.net",true},{"abc@gmail.com.com",true},{"abc+100@gmail.com",true},{"abc@yahoo.com",true},{"abc",false},{"abc@.com.my",false},{"abc123@gmail.a",false},{"abc123@.com",false},{"abc123@.com.com",false},{".abc@abc.com",false},{"abc()*@gmail.com",false},{"abc@%*.com",false},{"abc..2002@gmail.com",false},{"abc.@gmail.com",false},{"abc@abc@gmail.com",false},{"abc@gmail.com.1a",false},{"abc@gmail.com.aa.au",false}});
+        return Arrays.asList(new Object[][]{{"abc.100@abc.com.au",true},{"abc@yahoo.com",true},{"abc@yahoo.com",true},{"abc-100@yahoo.com",true},{"abc@1.com",true},{"abc111@abc.com",true},{"abc-100@abc.net",true},{"abc@gmail.com.com",true},{"abc+100@gmail.com",true},{"abc@yahoo.com",true},{"c",false},{"abc@.com.my",false},{"abc123@gmail.a",false},{"abc123@.com",false},{"abc123@.com.com",false},{".abc@abc.com",false},{"abc()*@gmail.com",false},{"abc@%*.com",false},{"abc..2002@gmail.com",false},{"abc.@gmail.com",false},{"abc@abc@gmail.com",false},{"abc@gmail.com.1a",false},{"abc@gmail.com.aa.au",false}});
     }
     @Test
     public void testEmail(){
